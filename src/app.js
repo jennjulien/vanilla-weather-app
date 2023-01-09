a3ba3de49004f3eabf71894f48ce3a2d956bef9b
jennjulien <116986260+jennjulien@users.noreply.github.com>
2023-01-09 15:27:23 -0500

    Add a new form

--- src/app.js
+++ src/app.js
@@ -41,10 +41,23 @@ function displayTemperature(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "52fa42dae9fecd18f16adcedc0a52231";
-let city = "New York";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}k&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "52fa42dae9fecd18f16adcedc0a52231";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}k&appid=${apiKey}&units=metric`;
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
 
 axios.get(url).then(displayTemperature);
+
+search("New York");
+
+let form = document.querySelector("search-form");
+form.addEventListener("submit", handleSubmit);
